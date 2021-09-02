f700b71a79fe7d53794b0525a4a025e4be5a9c24
Swannhs <princeapurbask@gmail.com>
2021-09-02 12:13:34 +0600

    Configured rest api to view all posts

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+from feed.models import Post
+from feed.serializers import GetPostsSerializer
+
+
+@api_view(['GET'])
+def get_posts(request):
+    posts = Post.objects.all()
+    serializer = GetPostsSerializer(posts, many=True)
+    return Response(serializer.data, HTTP_200_OK)
