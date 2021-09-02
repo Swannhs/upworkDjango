761abeb20f0d10020276d73f01c76dd81c62b23f
Swannhs <princeapurbask@gmail.com>
2021-09-02 12:11:53 +0600

    Modified post fields

--- feed/models.py
+++ feed/models.py
@@ -6,13 +6,13 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     title = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    createdAt = models.DateField(auto_now_add=True)
     tags = models.CharField(max_length=100, null=True, blank=True)
     vote = models.IntegerField(null=True, blank=True, default=0)
-    short_description = models.CharField(max_length=300)
-    description = models.TextField()
+    short_description = models.CharField(max_length=300, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     payment = models.BooleanField(default=False)
     reviews = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return str(self.title)
