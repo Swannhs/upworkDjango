f0c156624e0ac72cba982a054ae3ab6a79590590
Swannhs <princeapurbask@gmail.com>
2021-09-02 12:52:07 +0600

    Added url to create post

--- feed/urls.py
+++ feed/urls.py
@@ -5,4 +5,5 @@ from feed import views
 urlpatterns = [
     path('posts/', views.get_posts, name='Posts'),
     path('post/<str:id>', views.get_post, name='Post'),
+    path('create/', views.make_post, name='Create'),
 ]
