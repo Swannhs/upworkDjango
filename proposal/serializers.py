8308b607beea708df2d3e6733cb2f497fe5a452d
Swannhs <princeapurbask@gmail.com>
2021-09-03 13:00:20 +0600

    Added freelancer serializer as a user

---
+++ proposal/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class FreelancerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username'
+        ]
