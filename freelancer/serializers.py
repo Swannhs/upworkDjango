a1dc74aed9c371f519623687884cb5401c3e376a
Swannhs <princeapurbask@gmail.com>
2021-09-06 18:55:50 +0600

    Serializer for new User register and login

--- freelancer/serializers.py
+++ freelancer/serializers.py
@@ -1,11 +1,35 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from knox.models import AuthToken
+from knox.serializers import UserSerializer
+from rest_framework import serializers, generics
+from rest_framework.response import Response
 
 
 class FreelancerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = [
-            'id',
-            'username'
-        ]
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
