567d257785e3a307e1bbb6c669314d760098f47b
Swannhs <princeapurbask@gmail.com>
2021-09-06 18:50:23 +0600

    Added route url for login and registration

--- freelancer/urls.py
+++ freelancer/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from freelancer import views
+from freelancer.serializers import RegisterAPI
+from freelancer.views import LoginAPI
 
 urlpatterns = [
     path('', views.get_users, name='Freelancers'),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
 ]
