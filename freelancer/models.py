a25998a2a3cec60a12f03a34b095a804cf7b69c8
Swannhs <princeapurbask@gmail.com>
2021-09-06 18:48:24 +0600

    Not implemented yet

--- freelancer/models.py
+++ freelancer/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+# class User(AbstractUser):
+#     is_admin = models.BooleanField(default=False)
+#     is_freelancer = models.BooleanField(default=False)
+#     is_client = models.BooleanField(default=False)
+#
+#
+# class freelancer(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
